5f928dcbb6866840267be83d319f552b18761170
efehrholz <efehrhol@iu.edu>
2021-02-04 18:38:32 -0500

    example 1 outline?

--- labs/lab2/js1/app.js
+++ labs/lab2/js1/app.js
@@ -1,7 +1,20 @@
+var radius = 50;
+var x = -radius;
+var speed = 0.5;
+
 function setup() {
-    createCanvas(400, 300)
+    createCanvas(800, 600);
+    fill(136, 227, 203);
+    noStroke();
+    //ellipse(x, 300, 70, 70);
+    ellipseMode(RADIUS);
 }
 
 function draw() {
-    
+    background(224, 123, 0);
+    x += speed; //increase the valuse of x
+    if (x > width+radius) { //if the shape is off screen
+        x = radius; //move to the left edge
+    }
+    arc(x, 300, radius, radius, 0.52, 5.76)
 }
--- labs/lab2/js2/app.js
+++ labs/lab2/js2/app.js
@@ -1,7 +1,7 @@
 function setup() {
-    createCanvas(400, 400)
+    createCanvas(800, 600)
 }
 
 function draw() {
-    background(black)
+    background(50)
 }
