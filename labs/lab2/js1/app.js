1d89910a3c85febd1e2f5651a9f06b1991972fb9
efehrholz <efehrhol@iu.edu>
2021-02-04 18:40:34 -0500

    Update app.js

--- labs/lab2/js1/app.js
+++ labs/lab2/js1/app.js
@@ -6,15 +6,14 @@ function setup() {
     createCanvas(800, 600);
     fill(136, 227, 203);
     noStroke();
-    //ellipse(x, 300, 70, 70);
     ellipseMode(RADIUS);
 }
 
 function draw() {
     background(224, 123, 0);
-    x += speed; //increase the valuse of x
-    if (x > width+radius) { //if the shape is off screen
-        x = radius; //move to the left edge
+    x += speed;
+    if (x > width+radius) { 
+        x = radius; 
     }
     arc(x, 300, radius, radius, 0.52, 5.76)
 }
