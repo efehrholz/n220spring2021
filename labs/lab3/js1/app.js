3fd9f69ae326f81d263127fa5ba44d18a188a957
efehrholz <efehrhol@iu.edu>
2021-02-10 00:47:24 -0500

    update

---
+++ labs/lab3/js1/app.js
@@ -0,0 +1,15 @@
+let i = 0;
+
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    //fill(255);
+    while (i < 5) {
+    console.log(i);
+    i++;
+    }
+    console.log(i);
+}
