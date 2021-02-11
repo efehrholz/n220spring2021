7a7f263ef6e93b91d4a8b271f03c781055762adc
efehrholz <efehrhol@iu.edu>
2021-02-11 01:00:43 -0500

    FizzBuzz js

--- labs/lab3/js1/app.js
+++ labs/lab3/js1/app.js
@@ -1,12 +1,30 @@
-let x = 0;
+let x = 300;
 
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(1225, 600);
 }
 
 function draw() {
-    background(37, 41, 43);
+    //background(37, 41, 43);
     noStroke();
+    
+    for(let i=0; i<1225; i+=50) {
+        fill(0);
+        circle(i, x, 0)
+        if(i%3 == 0) {
+            fill(107, 78, 150);
+            circle(i, x, 50);
+        }
+    
+        if(i%5 == 0) {
+            fill(47, 130, 80);
+            rect(i+25, x-35, 75, 75);
+        }
 
+        if(i%3 == 0 && i%5 == 0) {
+            fill(44, 107, 156);
+            rect(i+25, x-35, 75, 75);
+        }
+    }
 }
