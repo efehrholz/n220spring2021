a05af0a1d775fe6fdfbe3a490e01c57bca8b5a8a
efehrholz <efehrhol@iu.edu>
2021-04-16 03:36:43 -0400

    lab 9

---
+++ labs/lab9/js1/app.js
@@ -0,0 +1,14 @@
+/*
+For the purposes of this exercise, bad words are: clear, water, tires.
+
+Create an application that:
+
+Takes input from a user using a simple text input field.
+Splits the string on spaces
+Loops through  the array looking for bad words in the array
+Adds 1 to a tally count when a bad word is found
+Outputs to the document
+If any bad words were found (found / not found)
+How many bad words, in total, were found
+Clears out the text field so the user can input a new string
+*/
